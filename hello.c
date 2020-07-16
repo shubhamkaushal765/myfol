3a87f4b96f87b34d101bd04254370a2a65abc5d1
Shubham <shubhamkaushal765@gmail.com>
2020-07-16 09:44:18 +0530

    Hello, World!

---
+++ hello.c
@@ -0,0 +1,13 @@
+/****************************************************************
+ * 		A simple "Hello, World!" program		*
+ * 		Author: Shubham Kaushal				*
+ * 		Purpose: Checking remote repository, Github	*
+ * 		Usage: NONE					*
+ ***************************************************************/
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
